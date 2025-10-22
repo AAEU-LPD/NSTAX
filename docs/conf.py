03cc52394260f544c460a729c25aecd1f849edb0
aaeu-systest <debasish.chanda@alpsalpine.com>
2025-10-22 17:53:53 +0200

    Init

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ author = 'AAEU-LPD'
 release = '0.1'
 
 sys.path.insert(0, os.path.abspath('..'))   # Add project root to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # -- General configuration ---------------------------------------------------
 extensions = [
