26407cd52cda2c38d3786a6f3dde6081de6261e0
aaeu-systest <debasish.chanda@alpsalpine.com>
2025-10-23 10:25:52 +0200

    Init

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ author = 'AAEU-LPD'
 release = '0.1'
 
 sys.path.insert(0, os.path.abspath('..'))   # Add project root to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # sys.path.insert(0, r'D:\a\NSTAX\NSTAX')
 # sys.path.insert(0, r'D:\a\NSTAX')
 
